353da14615981315e8a50a5e438b4f56ed45fe9f
Garrett <ggm6@zips.uakron.edu>
2019-02-06 21:47:19 -0500

    Tree Working

---
+++ Prj2/Prj2.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+#include <string>
+#include <stack>
+#include <vector>
+#include <assert.h>
+
+using namespace std;
+
+class node {
+public:
+	string Expression;
+
+	node * succ;
+
+	node() : succ(nullptr),Expression("") {};
+	node(string s) : succ(nullptr),Expression(s) {};
+	node(string s,node* p) : Expression(s),succ(p) {};
+};
+
+class binaryOp : public node {
+public:
+	node * leftChild;
+	node * rightChild;
+
+	binaryOp() : leftChild(nullptr),rightChild(nullptr) {};
+
+};
+
+
+void createAST(node*& head,const string& str);  // Essentially a wrapper
+void Vectorize(vector<string>& v, const string& str);  // Turns string expression into vector of strings
+void resolveExp(const vector<string>::iterator& end,node*& p,vector<string>::iterator& i); // Resolves expression of vector<string> into AST
+void evalAST(node*& p);
+bool recursiveEval(node*& p);
+
+
+
+int main() {
+
+	string exp{"!true||false&&false"};    // Result = false
+	//string exp{"!((!false)||true)"};    // Result = false
+	//string exp{"true"};                 // Result = true
+	node* treeHead = nullptr;
+	createAST(treeHead,exp);
+	evalAST(treeHead);
+	
+
+	return 0;
+}
+
+
+
+void Vectorize(vector<string>& v, const string& stmt) {
+	for (auto i=0; i<stmt.length(); ++i) {
+		if (stmt.substr(i,4)=="true") {
+			v.push_back("true");
+			i+=3;
+		}
+		else if (stmt.substr(i,5)=="false") {
+			v.push_back("false");
+			i+=4;
+		}
+		else if (stmt[i]=='!' || stmt[i]=='(' || stmt[i]==')') {
+			string s;
+			s.push_back(stmt[i]);
+			v.push_back(s);
+		}
+		else if (stmt.substr(i,2)=="||" || stmt.substr(i,2)=="&&") {
+			string s = stmt.substr(i,2);
+			v.push_back(s);
+			++i;
+		}
+	}
+}
+
+void createAST(node*& head,const string& str) {
+	vector<string> v;
+	Vectorize(v,str);
+	vector<string>::iterator i = v.begin();
+	vector<string>::iterator end = v.end();
+	resolveExp(end,head,i);
+}
+
+void resolveExp(const vector<string>::iterator& end,node*& p,vector<string>::iterator& i) {
+	if (*i=="!") {   
+		auto dummy = i+1;   // It was either throw "i+1" into the function using a cast, or use a dummy variable
+		if (dummy!=end)
+			resolveExp(end,p,dummy);
+		node* temp = p;
+		p = new node{*i,temp};
+		i = dummy+1;
+		if (i!=end)
+			resolveExp(end,p,i);
+	}
+	else if (*i=="true" || *i=="false") {
+		node* temp = p;
+		p = new node{*i,temp};
+		if (i+1!=end)
+			resolveExp(end,p,++i);
+	}
+	else if (*i=="||" || *i=="&&") {
+		node* temp = p;
+		p = new binaryOp;
+		p->Expression = *i;
+		static_cast<binaryOp*>(p)->leftChild = temp;
+		temp = nullptr;
+		if (i+1!=end)
+			resolveExp(end,temp,++i);
+		static_cast<binaryOp*>(p)->rightChild = temp;
+	}
+	else if (*i=="(" && i+1!=end)
+		resolveExp(end,p,++i);
+}
+
+bool recursiveEval(node*& p) {
+	if (p->Expression=="!")
+		return !recursiveEval(p->succ);
+	else if (p->Expression=="||")
+		return recursiveEval(static_cast<binaryOp*>(p)->leftChild) || recursiveEval(static_cast<binaryOp*>(p)->rightChild);
+	else if (p->Expression=="&&")
+		return recursiveEval(static_cast<binaryOp*>(p)->leftChild) && recursiveEval(static_cast<binaryOp*>(p)->rightChild);
+	else if (p->Expression=="true")
+		return true;
+	else if (p->Expression=="false")
+		return false;
+}
+
+void evalAST(node*& p) {   // Evaluation wrapper to house recursive function
+	bool ans = recursiveEval(p);
+	if (ans==true)
+		cout << "true" << endl;
+	else
+		cout << "false" << endl;
+}
